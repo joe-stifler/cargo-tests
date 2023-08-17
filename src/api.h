8f1d9d125d5d26ec3022c7bcc839b2977703c36a
Joe <joseribeiro1017@gmail.com>
2023-08-17 13:00:55 -0300

    feat: add api files

---
+++ src/api.h
@@ -0,0 +1,22 @@
+#ifndef API_H
+#define API_H
+
+/**
+ * Adds two integers.
+ *
+ * @param a The first integer.
+ * @param b The second integer.
+ * @return The sum of the two integers.
+ */
+int add(int a, int b);
+
+/**
+ * Subtracts two integers.
+ *
+ * @param a The first integer.
+ * @param b The second integer.
+ * @return The result of subtracting b from a.
+ */
+int subtract(int a, int b);
+
+#endif // API_H
