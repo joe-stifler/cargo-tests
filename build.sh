6d966d1974bfadd049fa4dc40d3570aec49163cb
Joe <joseribeiro1017@gmail.com>
2023-08-17 15:21:41 -0300

    ci: add build script

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p bin/
+
+# Compile the C code into an object file
+gcc -c -fPIC src/api.c -o bin/api.o
+
+# Create a shared library from the object file
+gcc -shared bin/api.o -o bin/libapi.so
+
+INCLUDE_FILE="api"
+INCLUDE_PATH="src"
+LIB_PATH="test-sys"
+RUST_PATH="$(pwd)/rust"
+RUST_INCLUDE_NAME="bindings.rs"
+bindgen --allowlist-file ${INCLUDE_PATH}/${INCLUDE_FILE}.h ${INCLUDE_PATH}/${INCLUDE_FILE}.h -o ${RUST_PATH}/${LIB_PATH}/src/${RUST_INCLUDE_NAME}
